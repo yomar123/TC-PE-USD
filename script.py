0c23ee0c13eda74f89e63d3157fe5be5befc7593
Yomar Vega <0332161046@unjfsc.edu.pe>
2022-09-28 00:39:16 -0500

    ver contenido web

--- script.py
+++ script.py
@@ -7,12 +7,31 @@ print()
 # url = "https://www.sbs.gob.pe/app/pp/SISTIP_PORTAL/Paginas/Publicacion/TipoCambioContable.aspx"
 
 url2 = 'https://api.github.com'
-
-
 response = requests.get(url2)
 
 if response.status_code == 200:
     print("Success!")
 else:
     print("An error has occurred.")
+
+# #Ver el contenido en bytes
+# Cod_Bytes = response.content
+# print(Cod_Bytes)
+# print()
+
+# #ver el contenido en cadena
+# Cod_Text = response.text
+# print(Cod_Text)
+# print()
+
+# #ver contenido  en codificacion explicita
+# response.encoding = 'utf-8'
+#Codific_Utf = response.text
+# print(Codific_Utf )
+
+#ver el contenido Json - Son diccionarios(Clave - Valor)
+# Diccionariojson = response.json()
+# print(Diccionariojson)
+# print()
+# print()
 

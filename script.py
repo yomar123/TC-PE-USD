6f1151722f93de71d10dc309fdd695096cf0fbaa
Yomar Vega <0332161046@unjfsc.edu.pe>
2022-09-28 00:11:52 -0500

    agregar if de Status code

--- script.py
+++ script.py
@@ -1,3 +1,18 @@
-#mi primer print
+import requests
 
 print("Hello, World")
+print()
+print()
+
+# url = "https://www.sbs.gob.pe/app/pp/SISTIP_PORTAL/Paginas/Publicacion/TipoCambioContable.aspx"
+
+url2 = 'https://api.github.com'
+
+
+response = requests.get(url2)
+
+if response.status_code == 200:
+    print("Success!")
+else:
+    print("Not Found!")
+
